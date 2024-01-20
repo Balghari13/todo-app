e54f39c3a33f374921b28db680d33a4d49462561
Balghari <najmbalghari13@gmail.com>
2024-01-20 22:02:52 +0500

    useRef
    used useRef for optimization

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 
 export const AddTodo = ({addBtn}) => {
-  const [name, setName] = useState('')
-  const [date, setDate] = useState('')
+  // const [name, setName] = useState('')
+  // const [date, setDate] = useState('')
+  const name = useRef();
+  const date = useRef();
   
   const handleAdd =()=>{
-    addBtn(name,date)
-    setDate('')
-    setName('')
+   const todoName = name.current.value
+   const todoDate = date.current.value
+    addBtn(todoName,todoDate)
+    name.current.value=''
+    date.current.value=''
+    // setDate('')
+    // setName('')
   }
   return (
     <div className='row'>
-    <input type="text" className='col-6' value={name} onChange={(e)=>setName(e.target.value)} />
-    <input type='date' className='col-4' value={date} onChange={(e)=>setDate(e.target.value)} />
+    <input type="text" className='col-6'
+    ref={name}
+    //don't need value and onchange when use useref
+   // value={name} onChange={(e)=>setName(e.target.value)}
+      />
+    <input type='date' className='col-4' 
+    ref={date}
+   // value={date} onChange={(e)=>setDate(e.target.value)}
+     />
     <button className='col-2 btn btn-success'
     onClick={handleAdd}
     >Add</button>
