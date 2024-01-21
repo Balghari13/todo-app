018e7e441e124db3666cc4f2a732199ada0f5012
Balghari <najmbalghari13@gmail.com>
2024-01-21 16:08:14 +0500

    update to separte component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
 
-import { useState } from 'react'
+
 import './App.css'
 import { AddTodo } from './components/AddTodo'
 import { Items } from './components/Items'
 import Welcome from './components/Welcome'
-import { TodoItemContext } from './store/todo-item-store'
+import TodoItemsContextProvider from './store/todo-item-store'
 
 
 function App() {
-
-  const [todoItems, setTodoItems] = useState([])
-    
-  // const addBtn = (name,date) =>{
-  //   const newItems = [...todoItems, {
-  //     name:name, date:date
-  //   }]
-  //   setTodoItems(newItems)
-  // }
-
-  //better way to write spread operator called functional update
-  const addBtn =(name,date)=>{
-    setTodoItems((currValue)=>[
-      ...currValue, {
-        name:name, 
-        date:date
-      }
-    ])
-  }
-  const delBtn = (todoName) =>{
-   // console.log('delet');
-    const newItem = todoItems.filter((item)=>item.name != todoName)
-    setTodoItems(newItem)
-  }
-  
   return (
-    <TodoItemContext.Provider value={{
-      todoItems,addBtn,delBtn
-    }}>
+    <TodoItemsContextProvider>
     <h2>Todo Items</h2>
      <AddTodo />
      <Welcome/>
      <Items />
-    </TodoItemContext.Provider>
+    </TodoItemsContextProvider>
   )
 }
 
--- src/store/todo-item-store.jsx
+++ src/store/todo-item-store.jsx
@@ -1,8 +1,73 @@
 import { createContext } from 'react';
-
+import { useReducer } from 'react'
 export const TodoItemContext = createContext({
   todoItems: [],
   addBtn: ()=>{},
   delBtn:()=>{}
 });
 
+const todoItemReducer = (currValue, action) =>{
+  let newTodoItem = currValue
+ if(action.type==='NEW_ITEM'){
+  newTodoItem=[
+    ...currValue, {
+      name:action.payload.name, 
+      date:action.payload.date
+    }
+  ]
+ }else if(action.type==='DEL_ITEM'){
+    newTodoItem = currValue.filter((item)=> item.name!=action.payload.itemName)
+ }
+ return newTodoItem
+}
+
+
+const TodoItemsContextProvider = ({children}) => {
+    //const [todoItems, setTodoItems] = useState([])
+    const [todoItems, dispatchItem]= useReducer(todoItemReducer,[])
+    
+    // const addBtn = (name,date) =>{
+    //   const newItems = [...todoItems, {
+    //     name:name, date:date
+    //   }]
+    //   setTodoItems(newItems)
+    // }
+  
+    //better way to write spread operator called functional update
+    const addBtn =(name,date)=>{
+      const newItemAction = {
+        type: 'NEW_ITEM',
+        payload:{
+          name,date
+        }
+      } 
+      dispatchItem(newItemAction)
+    }
+    const delBtn = (todoItemName) =>{
+     // console.log('delet');
+      // const newItem = todoItems.filter((item)=>item.name != todoName)
+      // setTodoItems(newItem)
+      const delItemAction = {
+        type: 'DEL_ITEM',
+        payload: {
+          itemName:todoItemName
+        }
+      }
+      dispatchItem(delItemAction)
+    }
+
+    return(
+      <TodoItemContext.Provider
+      value={{
+        todoItems,
+        addBtn,
+        delBtn
+      }}
+      >
+        {children}
+      </TodoItemContext.Provider>
+    )
+    
+}
+
+export default TodoItemsContextProvider
